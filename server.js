29504df1dfc8a8ef1e59b68fb0a1af0c54361db9
Mika Halla <mikahalla@gmail.com>
2023-02-21 19:13:48 +0100

    v4

--- server.js
+++ server.js
@@ -28,18 +28,11 @@ app.use('/api/pizza', pizzaRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/ingredients', ingredientRoutes);
 
-app.use(express.static('./client/dist'));
-// app.use(express.static(path.join(__dirname, './client/dist')));
+// app.use(express.static('./client/dist'));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(
-    path.resolve(
-      __dirname,
-      'var',
-      'task',
-      'client',
-      'dist',
-      'index.html'
-    ),
+    path.resolve(__dirname, 'client', 'dist', 'index.html'),
     (err) => {
       res.status(500).send(err);
     }
